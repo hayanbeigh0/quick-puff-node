30f15296ff99d5b3541b777dccc2e2a4dfbb0ae8
hayanbeigh <hayanbeigh117@gmail.com>
2024-09-06 20:33:15 +0530

    add: trust proxy

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-const express = require("express");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
-const cors = require("cors");
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
 
-const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController");
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 // const userRouter = require("./routes/userRoutes");
-const router = require("./routes/router");
+const router = require('./routes/router');
 
 const app = express();
+
+// Trust the reverse proxy (e.g., Vercel)
+app.set('trust proxy', true);
 
 // Middlewares
 // Set security HTTP headers
 app.use(helmet());
 
 // Development logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   window: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!",
+  message: 'Too many requests from this IP, please try again in an hour!',
 });
 
-app.use("/api", limiter);
+app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(
   express.json({
-    limit: "10kb",
-  })
+    limit: '10kb',
+  }),
 );
 
 // Data sanitization against NoSql query injection
@@ -49,16 +52,15 @@ app.use(xss());
 app.use(
   hpp({
     whitelist: [
-      "duration",
-      "ratingsAverage",
-      "ratingsQuantity",
-      "maxGroupSize",
-      "difficulty",
-      "price",
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
     ],
-  })
+  }),
 );
-
 
 // Implement CORS
 app.use(cors());
@@ -71,9 +73,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/v1/", router);
+app.use('/api/v1/', router);
 
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new AppError(`Can't find the ${req.originalUrl} on this server!`, 404));
 });
 
