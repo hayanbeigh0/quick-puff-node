a3585794a832a14850d4b3692262a1461827c099
Hayan Beigh <hayanbeigh117@gmail.com>
2024-10-31 13:05:12 +0530

    add: deep link check

--- app.js
+++ app.js
@@ -81,6 +81,61 @@ app.get('/', (req, res) => {
   res.send('Server has started!');
 });
 
+app.use((req, res, next) => {
+  const userAgent = req.headers['user-agent'];
+  console.log(userAgent);
+  const appStoreURL = 'https://testflight.apple.com/join/fwMRsTzw'; // Replace with your App Store URL
+  const playStoreURL =
+    'https://play.google.com/store/apps/details?id=YOUR_PACKAGE_NAME'; // Replace with your Play Store URL
+  const deepLinkURL = 'yourappscheme://'; // Replace with your app's custom URL scheme
+
+  // Check if the request path is intended for deep linking
+  if (req.path.includes('https://quick-puff-node.onrender.com')) {
+    // Adjust this check based on your deep link structure
+    if (/iPhone|iPad|iPod/i.test(userAgent)) {
+      // iOS device
+      res.send(`
+        <html>
+          <head>
+            <title>Redirecting...</title>
+          </head>
+          <body>
+            <script type="text/javascript">
+              window.location.href = '${deepLinkURL}';
+              setTimeout(function () {
+                window.location.href = '${appStoreURL}';
+              }, 500);
+            </script>
+          </body>
+        </html>
+      `);
+    } else if (/Android/i.test(userAgent)) {
+      // Android device
+      res.send(`
+        <html>
+          <head>
+            <title>Redirecting...</title>
+          </head>
+          <body>
+            <script type="text/javascript">
+              window.location.href = '${deepLinkURL}';
+              setTimeout(function () {
+                window.location.href = '${playStoreURL}';
+              }, 500);
+            </script>
+          </body>
+        </html>
+      `);
+    } else {
+      // For other devices or if detection fails, redirect to a landing page or app website
+      res.redirect('https://yourappwebsite.com'); // Replace with your website or fallback page
+    }
+  } else {
+    // If the path is not a deep link, proceed as usual
+    next();
+  }
+});
+
 app.use('/api/v1/', router);
 
 app.all('*', (req, res, next) => {
