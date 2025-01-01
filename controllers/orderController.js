e9251f7ffcf96665f7d63a36d8f6a236871e2a04
Hayan Beigh <hayanbeigh117@gmail.com>
2025-01-01 13:08:27 +0530

    add: webhook secret to .env

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -304,7 +304,7 @@ const createOrder = setTransaction(async (req, res, next, session) => {
   });
 });
 
-const getAdditionalCharges = catchAsync(async (req, res, next) => {
+const getAdditionalCharges = setTransaction(async (req, res, next, session) => {
   const userId = req.user._id;
   const user = await User.findById(userId);
   if (!user || !user.dateOfBirth) {
